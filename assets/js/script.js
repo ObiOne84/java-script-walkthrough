8df12bdc50972c215471af10d000c6a0173e2c7d
ObiOne84 <>
2023-05-28 12:15:10 +0000

    Set structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incremenetScore()  {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubstractQuestion() {
+
+}
+
+function dispalyMultiplyQuestion() {
+    
+}
+
